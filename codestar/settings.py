e1e53ef1c4c4766a051229d6f6467cf1575c4262
adrianleung5 <adrianleung50@gmail.com>
2023-04-12 23:40:36 +0000

    up to create adminsite part 1

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['blog_adrian.herokuapp.com', 'localhost' ]
+ALLOWED_HOSTS = ['blog-adrian.herokuapp.com', 'localhost' ]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote', 
     'blog'
     
 ]
